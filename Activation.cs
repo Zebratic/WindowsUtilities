ffff3c1fbb2d5c32a06a5fecbbe2dd393839dc96
Zebratic <zebraticyt@gmail.com>
2022-03-22 00:17:41 +0100

    Initial commit

---
+++ Activation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsUtilities
+{
+    class Activation
+    {
+        public static void ActivateWindowsHome()
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C slmgr //B /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 & slmgr //B /skms kms8.msguides.com & slmgr //B /ato";
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public static void ActivateWindowsHomeN()
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C slmgr //B /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM & slmgr //B /skms kms8.msguides.com & slmgr //B /ato";
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public static void ActivateWindowsPro()
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C slmgr //B /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX & slmgr //B /skms kms8.msguides.com & slmgr //B /ato";
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public static void ActivateWindowsProN()
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C slmgr //B /ipk MH37W-N47XK-V7XM9-C7227-GCQG9 & slmgr //B /skms kms8.msguides.com & slmgr //B /ato";
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public static void ActivateWindowsEnterprise()
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C slmgr //B /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43 & slmgr //B /skms kms8.msguides.com & slmgr //B /ato";
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsUtilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu:
+            BetterConsole.Clear();
+            BetterConsole.WriteLine("Welcome to Windows Utitlites!", ConsoleColor.Cyan, true);
+            BetterConsole.WriteLine();
+            BetterConsole.WriteNumber(1, "Activate Windows");
+            BetterConsole.WriteNumber(2, "Upgrade Windows");
+            BetterConsole.WriteNumber(3, "Manage Administrator Account");
+            ConsoleKeyInfo key = BetterConsole.ReadKey();
+            if (key.Key == ConsoleKey.D1)
+            {
+                ActivateWindowsMenu();
+            }
+            else if (key.Key == ConsoleKey.D2)
+            {
+
+            }
+            else if (key.Key == ConsoleKey.D3)
+            {
+
+            }
+            goto menu;
+        }
+
+        public static void ActivateWindowsMenu()
+        {
+            menu:
+            BetterConsole.Clear();
+            BetterConsole.WriteLine("Activate Windows", ConsoleColor.Cyan, true);
+            BetterConsole.WriteLine();
+            BetterConsole.WriteNumber(1, "Windows 10 Home");
+            BetterConsole.WriteNumber(2, "Windows 10 Home N");
+            BetterConsole.WriteNumber(3, "Windows 10 Pro");
+            BetterConsole.WriteNumber(4, "Windows 10 Pro N");
+            BetterConsole.WriteNumber(5, "Windows 10 Enterprise");
+            BetterConsole.WriteNumber(6, "Back");
+            ConsoleKeyInfo key = BetterConsole.ReadKey();
+            if (key.Key == ConsoleKey.D1)
+            {
+                BetterConsole.Clear();
+                BetterConsole.WriteLine("Activating Windows 10 Home... Please wait...", ConsoleColor.Cyan);
+                Activation.ActivateWindowsHome();
+                BetterConsole.WriteLine("Done! Please check if your windows is activated!", ConsoleColor.Cyan);
+                BetterConsole.ReadLine();
+            }
+            else if (key.Key == ConsoleKey.D2)
+            {
+                BetterConsole.Clear();
+                BetterConsole.WriteLine("Activating Windows 10 Home N... Please wait...", ConsoleColor.Cyan);
+                Activation.ActivateWindowsHomeN();
+                BetterConsole.WriteLine("Done! Please check if your windows is activated!", ConsoleColor.Cyan);
+                BetterConsole.ReadLine();
+            }
+            else if (key.Key == ConsoleKey.D3)
+            {
+                BetterConsole.Clear();
+                BetterConsole.WriteLine("Activating Windows 10 Pro... Please wait...", ConsoleColor.Cyan);
+                Activation.ActivateWindowsPro();
+                BetterConsole.WriteLine("Done! Please check if your windows is activated!", ConsoleColor.Cyan);
+                BetterConsole.ReadLine();
+            }
+            else if (key.Key == ConsoleKey.D4)
+            {
+                BetterConsole.Clear();
+                BetterConsole.WriteLine("Activating Windows 10 Pro N... Please wait...", ConsoleColor.Cyan);
+                Activation.ActivateWindowsProN();
+                BetterConsole.WriteLine("Done! Please check if your windows is activated!", ConsoleColor.Cyan);
+                BetterConsole.ReadLine();
+            }
+            else if (key.Key == ConsoleKey.D5)
+            {
+                BetterConsole.Clear();
+                BetterConsole.WriteLine("Activating Windows 10 Enterprise... Please wait...", ConsoleColor.Cyan);
+                Activation.ActivateWindowsEnterprise();
+                BetterConsole.WriteLine("Done! Please check if your windows is activated!", ConsoleColor.Cyan);
+                BetterConsole.ReadLine();
+            }
+            else if (key.Key == ConsoleKey.D6)
+            {
+                return;
+            }
+            goto menu;
+        }
+    }
+}
